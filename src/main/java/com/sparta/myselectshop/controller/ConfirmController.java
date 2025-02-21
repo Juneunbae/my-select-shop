ab51322163a4eadff2a8b71d060947f8ff223103
juneunbae <dmsqo1403@naver.com>
2025-02-21 23:22:31 +0900

    블루 테스트

--- src/main/java/com/sparta/myselectshop/controller/ConfirmController.java
+++ src/main/java/com/sparta/myselectshop/controller/ConfirmController.java
@@ -1,18 +1,17 @@
 package com.sparta.myselectshop.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-public class HomeController {
+@RestController
+public class ConfirmController {
     @Value("${spring.profiles.active}")
     private String activeProfile;
 
-
-    @GetMapping("/")
-    public String home() {
+    @GetMapping("/confirm")
+    public String confirm() {
         System.out.println(activeProfile);
-        return "index";
+        return activeProfile;
     }
 }
